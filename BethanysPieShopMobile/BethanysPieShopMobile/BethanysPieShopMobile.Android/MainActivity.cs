02bdd77b2e02fe4102f41cc3d8f92e4eb08b7cea
vicluar <vicluar@gmail.com>
2019-04-28 18:40:00 +0200

    Removing test changes.

--- BethanysPieShopMobile/BethanysPieShopMobile/BethanysPieShopMobile.Android/MainActivity.cs
+++ BethanysPieShopMobile/BethanysPieShopMobile/BethanysPieShopMobile.Android/MainActivity.cs
@@ -20,8 +20,6 @@ namespace BethanysPieShopMobile.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-
-
         }
     }
 }
